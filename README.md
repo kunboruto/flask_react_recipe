dfc9b3723561f3f3638f885f39c4f94fa7236029
kunboruto <kunboruto6@gmail.com>
2022-07-22 16:31:14 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# HR MANAGEMENT SYSTEM
+
+HR MANAGEMENT SYSTEM is a web-based application that is useful for helping the HR work.
+
+## Installation For Development
+
+Path (on Mr. Adi's Personal Computer):
+
+```bash
+D:\kelvin\hr_management_system
+```
+
+Create virtual environment and activate:
+
+```bash
+D:
+
+cd /kelvin/hr_management_system
+
+python -m venv venv
+
+cd /venv/Scripts/activate.bat
+```
+
+Make and install requirements:
+
+```bash
+pip freeze > requirements.txt
+
+pip install -r requirements.txt
+```
---
+++ config.py
@@ -0,0 +1,28 @@
+from decouple import config
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class Config:
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'test.db')
+    SQLALCHEMY_ECHO = False
+    TESTING = True
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'test.db')
+    SQLALCHEMY_ECHO = config('ECHO')
+    DEBUG = config('DEBUG')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS')
